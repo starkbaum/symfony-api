8293046f5c08debcdb2c891203bd223a74add011
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-02 16:09:26 +0100

    Chapter 23: adding items to a collection property

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -53,14 +53,14 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
-     * @Groups({"user:write", "user:read", "cheese_listing:item:get"})
+     * @Groups({"user:write", "user:read", "cheese_listing:item:get", "cheese_listing:write"})
      * @Assert\NotBlank()
      */
     private $username;
 
     /**
      * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner")
-     * @Groups({"user:read"})
+     * @Groups({"user:read", "user:write"})
      */
     private $cheeseListings;
 
