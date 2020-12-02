21da9ace03053bc68524d2a1c13a03975e2ed973
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-02 13:50:08 +0100

    Chapter 13: propertyfilter sparse fieldsets

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -7,6 +7,8 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -27,6 +29,8 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
  * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
  * @ApiFilter(SearchFilter::class, properties={"title": "partial", "description"="partial"})
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ApiFilter(PropertyFilter::class)
  */
 class CheeseListing
 {
@@ -90,6 +94,20 @@ class CheeseListing
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    /**
+     * @Groups("cheese_listing:read")
+     *
+     * @return string|null
+     */
+    public function getShortDescription(): ?string
+    {
+        if (strlen($this->getDescription()) > 40) {
+            return $this->getDescription();
+        }
+
+        return substr($this->getDescription(), 0, 40) . '..';
     }
 
     public function setDescription(string $description): self
